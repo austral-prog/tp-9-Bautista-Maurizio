abdaeb8b7b18cc2f8abb44fba172a97786f81fc5
Bautista-Maurizio <83047373+Bautista-Maurizio@users.noreply.github.com>
2024-05-31 10:12:42 -0300

    Update dicts.py

--- dicts.py
+++ dicts.py
@@ -1,48 +1,44 @@
 
 def create_inventory(items):
-    """Create a dict that tracks the amount (count) of each element on the `items` list.
+    inventory = dict()
+    for item in items:
+        if item not in inventory:
+            inventory[item] = 1
+        else:
+            inventory[item] += 1
+    return inventory
 
-    :param items: list - list of items to create an inventory from.
-    :return: dict - the inventory dictionary.
-    """
-    return {}
+
 
 
 def add_items(inventory, items):
-    """Add or increment items in inventory using elements from the items `list`.
+    for item in items:
+        if item not in inventory:
+            inventory[item] = 1
+        else:
+            inventory[item] += 1
+    return inventory
 
-    :param inventory: dict - dictionary of existing inventory.
-    :param items: list - list of items to update the inventory with.
-    :return: dict - the inventory updated with the new items.
-    """
-    return {}
+
+   
 
 
 def decrement_items(inventory, items):
-    """Decrement items in inventory using elements from the `items` list.
+    for item in items:
+        if item in inventory and inventory[item] > 0:
+            inventory[item] -= 1
+    return inventory
 
-    :param inventory: dict - inventory dictionary.
-    :param items: list - list of items to decrement from the inventory.
-    :return: dict - updated inventory with items decremented.
-    """
-    return {}
 
 
 def remove_item(inventory, item):
-    """Remove item from inventory if it matches `item` string.
-
-    :param inventory: dict - inventory dictionary.
-    :param item: str - item to remove from the inventory.
-    :return: dict - updated inventory with item removed. Current inventory if item does not match.
-    """
-    return {}
+    if item in inventory:
+        del inventory[item]
+    if item not in inventory:
+        return inventory
 
 
 def list_inventory(inventory):
-    """Create a list containing all (item_name, item_count) pairs in inventory.
-
-    :param inventory: dict - an inventory dictionary.
-    :return: list of tuples - list of key, value pairs from the inventory dictionary.
-    """
-    return {}
+    Tupla = [(item, cantidad) for item, cantidad in inventory.items() if cantidad>0]
+    return Tupla
 
